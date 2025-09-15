9029c8efa69df0c225f26b13a057dd7c6c4c5644
jk2k <4025839+jk2k@users.noreply.github.com>
2025-09-15 21:28:46 +0800

    fix: change brand to HarborNAS

--- src/app/helpers/copyright-text.helper.ts
+++ src/app/helpers/copyright-text.helper.ts
@@ -3,7 +3,7 @@ import { ProductType, productTypeLabels } from 'app/enums/product-type.enum';
 
 export function getCopyrightHtml(productType?: ProductType): string {
   if (productType) {
-    return `TrueNAS® ${productTypeLabels.get(productType)} <br /> © ${environment.buildYear}`;
+    return `HarborNAS® <br /> © ${environment.buildYear}`;
   }
-  return `TrueNAS® <br /> © ${environment.buildYear}`;
+  return `HarborNAS® <br /> © ${environment.buildYear}`;
 }
